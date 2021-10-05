c2d341a8d34ecf8144c65c7d09148c51546edfe1
Mohit Talniya <mohittalniya@gmail.com>
2021-10-05 20:46:39 +0530

    Update cryto_coin.go
    updated entity

--- internal/crypto/entity/cryto_coin.go
+++ internal/crypto/entity/cryto_coin.go
@@ -5,5 +5,23 @@ package entity
 
 /// enum for coin types
 
-type crypto struct {
+type CoinType int
+
+
+
+
+type Crypto struct {
+	coinType CoinType
+	buyPrice float64
+	sellPrice float64
+	spotPrice float64
+}
+
+func NewCrypto(coinType CoinType, buyPrice, sellPrice, spotPrice float64) Crypto {
+	return Crypto{
+		coinType:  coinType,
+		buyPrice:  buyPrice,
+		sellPrice: sellPrice,
+		spotPrice: spotPrice,
+	}
 }
